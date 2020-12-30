c0645bb8e54936764fbe34074e3f3a0752e9f01b
islam <islam.elsayed101@gmail.com>
2020-12-30 21:14:24 +0200

    project that crawl products info from jumia site and add them to database.

---
+++ crawl data from jumia using selenium/config.py
@@ -0,0 +1,8 @@
+jumia_base_url    = 'https://www.jumia.com.eg/ar'
+categories        = ['phones-tablets','electronics']
+gecko_driver      = 'driver/geckodriver.exe'
+user_agent        = 'Mozilla/5.0 (Windows NT 10.0; WOW64; rv:50.0) Gecko/20100101 Firefox/50.0'
+page_load_timeout = 30
+database_host     = 'localhost'
+database_user     = 'root'
+database_pass     = ''
